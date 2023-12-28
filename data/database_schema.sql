db0a6f7f529e430e358ba63abd11083c939548c7
RakeshGoud <112722271+rgd1489@users.noreply.github.com>
2023-12-28 21:50:32 +0200

    Create database_schema.sql

---
+++ data/database_schema.sql
@@ -0,0 +1,25 @@
+-- data/database_schema.sql
+CREATE TABLE IF NOT EXISTS user_accounts (
+    id SERIAL PRIMARY KEY,
+    game_name VARCHAR(10),
+    user_id VARCHAR(50),
+    gender VARCHAR(10),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    location_street VARCHAR(255),
+    location_city VARCHAR(100),
+    location_state VARCHAR(100),
+    location_postcode VARCHAR(20),
+    dob TIMESTAMP,
+    registered TIMESTAMP,
+    phone VARCHAR(20),
+    cell VARCHAR(20),
+    country_code VARCHAR(10),
+    picture_large VARCHAR(255),
+    picture_medium VARCHAR(255),
+    picture_thumbnail VARCHAR(255),
+    country_code VARCHAR(50),
+    country_name VARCHAR(50),
+    location VARCHAR(255)
+);
